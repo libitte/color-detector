0522e2d043dfc9830282cd82127d8418e77398c2
libitte <libitte3@gmail.com>
2015-04-12 02:13:35 +0900

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name colorDetectorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the colorDetectorApp
+ */
+angular.module('colorDetectorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name colorDetectorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the colorDetectorApp
+ */
+angular.module('colorDetectorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
